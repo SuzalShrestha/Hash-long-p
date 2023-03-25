53a803faa30f5e3545f123edbca68bb122cc4953
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-03-25 14:12:31 +0545

    added longestSubstr bonus

--- phases/03-bonus.js
+++ phases/03-bonus.js
@@ -80,3 +80,46 @@ let longestSubstr=(str)=>{
     }));
     return occurence.length;
 }
+let maxSubarr=(arr)=>{
+    let obj={};
+    //list all occurence of each number
+    for(let i=0;i<arr.length;i++){
+        if(obj[arr[i]]){
+            obj[arr[i]]++;
+        }else{
+            obj[arr[i]]=1;
+        }
+    }
+    let keys=Object.keys(obj);
+    let pairs=[];
+    //find all pairs of numbers that are 1 apart
+    for(let i=0;i<keys.length;i++){
+        let current=keys[i];
+        for(let j=i+1;j<keys.length;j++){
+            if(Math.abs(current-keys[j])<=1){
+                pairs.push([current,keys[j]]);
+            }
+        }
+    }
+    //find the longest subarray
+    if(pairs.length===0){
+        return 0;
+    }
+    let longestpair=pairs[0];
+    let longest=obj[longestpair[0]]+obj[longestpair[1]];
+    if(pairs.length===1){
+        return longest;
+    }
+    for(let i=1;i<pairs.length-1;i++){
+        let current=pairs[i];
+        let currentlength=obj[current[0]]+obj[current[1]];
+        if(currentlength>longest){
+            longestpair=current;
+            longest=currentlength;
+        }
+    } 
+       return longest;
+}
+
+console.log(maxSubarr([1,3,2,2,5,2,3,7])); // => 5 because the longest subarray is [3,2,2,2,3]
+console.log(maxSubarr([1,1,1,1,3]));// => 4 because the longest subarray is [1,1,1,1]
